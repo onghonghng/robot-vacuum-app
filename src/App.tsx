7783514ba0e06967fcd7171c89751d246d899519
onghonghng <onghonghng@gmail.com>
2023-04-20 16:42:30 +0800

    New Feature: Added Material UI library and sample code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <TextField id="input-command" label="Command" variant="standard" multiline />
+
       </header>
     </div>
   );
